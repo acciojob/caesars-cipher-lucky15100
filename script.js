9d94a2bf20b9aa28303c81dd8c3ec2f17819b66d
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-07-08 23:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
-// Your Script here.
+function rot13(encodedString) {
+  var decodedString = "";
 
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
+  for (var i = 0; i < encodedString.length; i++) {
+    var charCode = encodedString.charCodeAt(i);
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+    if (charCode >= 65 && charCode <= 90) {
+      // If it's an uppercase letter (A-Z)
+      var decodedCharCode = ((charCode - 65 + 13) % 26) + 65;
+      decodedString += String.fromCharCode(decodedCharCode);
+    } else {
+      // Non-alphabetic characters
+      decodedString += encodedString.charAt(i);
+    }
+  }
 
-  return; //return decodedArr
+  return decodedString;
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
